19303b035edfc12334d8382f6583347a78d9deea
조성원 <tjddnjsjo@kakao.com>
2023-07-28 18:55:40 +0900

    feat: 이미지 리사이징 구현

---
+++ app/backend/resize-image/index.py
@@ -0,0 +1,38 @@
+import json
+import boto3
+import os
+from PIL import Image
+from io import BytesIO
+
+s3 = boto3.client("s3")
+
+
+def resize_image(image_data):
+    image = Image.open(BytesIO(image_data))
+    new_width = 300
+    new_height = int(image.height * (new_width / image.width))
+    resized_image = image.resize((new_width, new_height))
+
+    output_buffer = BytesIO()
+    resized_image.save(output_buffer, format="JPEG")
+    return output_buffer.getvalue()
+
+
+def lambda_handler(event, context):
+    try:
+        bucket = event["Records"][0]["s3"]["bucket"]["name"]
+        key = event["Records"][0]["s3"]["object"]["key"]
+
+        response = s3.get_object(Bucket=bucket, Key=key)
+        image_data = response["Body"].read()
+
+        resized_image = resize_image(image_data)
+
+        target_bucket = os.environ["TARGET_BUCKET"]
+        s3.put_object(Bucket=target_bucket, Key=key, Body=resized_image)
+
+        return {"statusCode": 200, "body": json.dumps("Image resized successfully!")}
+
+    except Exception as e:
+        print("Error:", e)
+        return {"statusCode": 500, "body": json.dumps("Image resizing failed.")}
--- cdk/lib/stack/lambda-stack.ts
+++ cdk/lib/stack/lambda-stack.ts
@@ -5,6 +5,12 @@ import {
   ServicePrincipal,
   CompositePrincipal,
 } from "aws-cdk-lib/aws-iam";
+import { LayerVersion, Runtime } from "aws-cdk-lib/aws-lambda";
+import { PythonFunction } from "@aws-cdk/aws-lambda-python-alpha";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3n from "aws-cdk-lib/aws-s3-notifications";
+
+import * as path from "path";
 
 import { Construct } from "constructs";
 
@@ -29,6 +35,39 @@ export class PictatoLambdaStack extends cdk.Stack {
       ],
     });
 
-    // function here
+    // 이미지 리사이징
+    const bucket = s3.Bucket.fromBucketName(
+      this,
+      "ExistingBucket",
+      `${getAccountUniqueName(props.context)}-pictato-bucket`.toLowerCase()
+    );
+    const imageResizerFunction = new PythonFunction(
+      this,
+      `${SYSTEM_NAME}-resize-image`,
+      {
+        functionName: `${getAccountUniqueName(props.context)}-resize-image`,
+        runtime: Runtime.PYTHON_3_10,
+        entry: path.join(__dirname, "../../../app/backend/resize-image"),
+        index: "index.py",
+        handler: "lambda_handler",
+        role: lambdaRole,
+        timeout: cdk.Duration.seconds(30),
+        environment: {
+          TARGET_BUCKET: bucket.bucketName,
+        },
+      }
+    );
+    imageResizerFunction.addLayers(
+      LayerVersion.fromLayerVersionArn(
+        this,
+        "Pillow",
+        "arn:aws:lambda:ap-northeast-2:770693421928:layer:Klayers-p310-Pillow:2"
+      )
+    );
+    bucket.grantReadWrite(imageResizerFunction);
+    bucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
+      new s3n.LambdaDestination(imageResizerFunction)
+    );
   }
 }
