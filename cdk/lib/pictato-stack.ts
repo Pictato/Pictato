623fdbb10b31baa3684500d8caa3f158ef8379b7
J2yun <alex68566856@gmail.com>
2023-07-29 16:45:20 +0900

    dynamo stack 추가

--- cdk/lib/pictato-stack.ts
+++ cdk/lib/pictato-stack.ts
@@ -4,12 +4,14 @@ import { Construct } from "constructs";
 
 import { PictatoS3Stack } from "./stack/s3-stack";
 import { PictatoLambdaStack } from "./stack/lambda-stack";
+import { PictatoDynamoStack } from "./stack/dynamo-stack";
 import { Account } from "./config/accounts";
 import { SYSTEM_NAME } from "./config/commons";
 
 export interface PictatoStackProps extends cdk.StackProps {
   context: Account;
   s3Stack?: PictatoS3Stack;
+  dynamoStack? : PictatoDynamoStack;
 }
 
 export class PictatoStack extends cdk.Stack {
@@ -18,6 +20,9 @@ export class PictatoStack extends cdk.Stack {
 
     const s3Stack = new PictatoS3Stack(this, `${SYSTEM_NAME}-s3Stack`, props);
     props.s3Stack = s3Stack;
+
+    const dynamoStack = new PictatoDynamoStack(this, `${SYSTEM_NAME}-dynamoStack`, props)
+    props.dynamoStack = dynamoStack;
 
     new PictatoLambdaStack(this, `${SYSTEM_NAME}-lambdaStack`, props);
   }
