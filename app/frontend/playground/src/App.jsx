146769aee55f35fe502023826f6c45f8aab652c9
조성원 <tjddnjsjo@kakao.com>
2023-07-30 23:54:58 +0900

    feat: 메인 페이지 제작

--- app/frontend/playground/src/App.jsx
+++ app/frontend/playground/src/App.jsx
@@ -1,18 +1,31 @@
 import { useState } from "react";
 
+import Pictato from "./assets/pictato.png";
+
 const App = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>Playground</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure className="px-10 pt-10">
+        <img src={Pictato} alt="Pictato" className="rounded-xl" />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">
+          Pictato
+          <div className="badge badge-secondary">Playground</div>
+        </h2>
         <p>React playground for Pictato with Amazon Web Services</p>
+        <div className="card-actions">
+          <button
+            className="btn btn-primary"
+            onClick={() => setCount((count) => count + 1)}
+          >
+            count is {count}
+          </button>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
