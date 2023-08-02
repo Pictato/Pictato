3bbd96d780922b5b97629eace9fecd6899b4a3f3
조성원 <tjddnjsjo@kakao.com>
2023-08-02 20:56:21 +0900

    feat: GetTest 추가

--- app/frontend/playground/src/App.jsx
+++ app/frontend/playground/src/App.jsx
@@ -1,11 +1,13 @@
 import Main from "./components/Main";
 import PostTest from "./components/PostTest";
+import GetTest from "./components/GetTest";
 
 const App = () => {
   return (
     <div className="flex p-4 gap-4">
       <Main />
       <PostTest />
+      <GetTest />
     </div>
   );
 };
---
+++ app/frontend/playground/src/components/GetTest.jsx
@@ -0,0 +1,64 @@
+import { useState, useRef } from "react";
+
+import { dynamoApi } from "../apis/dynamoApi";
+
+const GetTest = () => {
+  const [data, setData] = useState([]);
+  const userIdRef = useRef("");
+
+  const handleGetRequest = async () => {
+    try {
+      const res = await dynamoApi.readPost(userIdRef.current.value);
+      console.log(res.data.body);
+      setData(res.data.body);
+
+      alert("GET에 성공했습니다.");
+    } catch (err) {
+      alert(`GET에 실패했습니다. (${err})`);
+    }
+  };
+
+  return (
+    <div className="card bg-base-100 shadow-xl">
+      <div className="card-body">
+        <h2 className="card-title">
+          <div className="join w-full">
+            <input
+              className="input input-bordered join-item w-full"
+              placeholder="Username"
+              ref={userIdRef}
+            />
+            <button
+              className="btn btn-secondary join-item rounded-r-full"
+              onClick={handleGetRequest}
+            >
+              GET
+            </button>
+          </div>
+        </h2>
+        <table className="table table-zebra">
+          <thead>
+            <tr>
+              <th>Index</th>
+              <th>Username</th>
+              <th>File</th>
+              <th>Memo</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={`item-${index}`}>
+                <th>{item.index}</th>
+                <td>{item["user-id"]}</td>
+                <td>{item["file-name"]}</td>
+                <td>{item.memo}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default GetTest;
