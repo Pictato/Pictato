7cd1f8a8223d78db457bd768f6e01402f034b122
조성원 <tjddnjsjo@kakao.com>
2023-08-02 20:57:00 +0900

    feat: Username, index 입력란 추가

--- app/frontend/playground/src/components/PostTest.jsx
+++ app/frontend/playground/src/components/PostTest.jsx
@@ -3,19 +3,21 @@ import { useRef } from "react";
 import { dynamoApi } from "../apis/dynamoApi";
 
 const PostTest = () => {
+  const userIdRef = useRef("");
+  const indexRef = useRef(0);
   const fileRef = useRef(undefined);
   const memoRef = useRef("");
 
-  const handlePost = async () => {
+  const handlePostRequest = async () => {
     try {
       const DYNAMO_DATA = {
-        userId: "test",
-        index: "1",
+        userId: userIdRef.current.value,
+        index: indexRef.current.value,
         fileName: fileRef.current.files[0].name,
         memo: memoRef.current.value,
       };
 
-      console.log(await dynamoApi.createPost(DYNAMO_DATA));
+      await dynamoApi.createPost(DYNAMO_DATA);
 
       alert("POST에 성공했습니다.");
     } catch (err) {
@@ -28,7 +30,17 @@ const PostTest = () => {
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <div className="card-body">
-        <h2 className="card-title">
+        <h2 className="card-title flex-col">
+          <input
+            className="input input-bordered w-full"
+            placeholder="Username"
+            ref={userIdRef}
+          />
+          <input
+            className="input input-bordered w-full"
+            placeholder="Index"
+            ref={indexRef}
+          />
           <input
             type="file"
             className="file-input file-input-bordered w-full max-w-xs"
@@ -40,7 +52,7 @@ const PostTest = () => {
           placeholder="Memo"
           ref={memoRef}
         ></textarea>
-        <div className="card-actions justify-end" onClick={handlePost}>
+        <div className="card-actions justify-end" onClick={handlePostRequest}>
           <button className="btn btn-primary">Add</button>
         </div>
       </div>
