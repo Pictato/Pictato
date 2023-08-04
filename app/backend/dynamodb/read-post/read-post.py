8cd1f50667317218a90d6cc69d6206824d787a4b
J2yun <alex68566856@gmail.com>
2023-08-04 13:43:25 +0900

    refactor: Lambda함수 api gateway 맞춰 수정

--- app/backend/dynamodb/read-post/read-post.py
+++ app/backend/dynamodb/read-post/read-post.py
@@ -7,19 +7,25 @@ import io
 def lambda_handler(event, context):
     target_table = io.envrion["TARGET_TABLE"]
 
-    response_body = json.loads(event["body"])
-
     dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table(target_table)
 
     # 데이터 조회하기 위한 user-id 받아오기
-    userID = response_body["user-id"]
+    try:
+        userID = event["userId"]
+    except:
+        return {
+            "statusCode": 500,
+            "body": json.dumps("userID loading error!"),
+        }
 
-    # user-id 속성이 같은 놈들 return
-    query = table.query(KeyConditionExpression=Key("user-id").eq(userID))
+    try:
+        # user-id 속성이 같은 놈들 return
+        query = table.query(KeyConditionExpression=Key("user-id").eq(userID))
+        # user-id 파티션의 각 튜플들을 dic 형태로 가지는 list
+        # [{}, {}, {}]
+        items = query["Items"]
 
-    # user-id 파티션의 각 튜플들을 dic 형태로 가지는 list
-    # [{}, {}, {}]
-    items = query["Items"]
-
-    return {"statusCode": 200, "body": json.dumps(items)}
+        return {"statusCode": 200, "body": items}
+    except:
+        return {"statusCode": 500, "body": json.dumps("query problem")}
