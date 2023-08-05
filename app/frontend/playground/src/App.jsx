cb8213065361effc89ea311ceb2457e420fdc787
조성원 <tjddnjsjo@kakao.com>
2023-08-05 16:16:49 +0900

    design: Divider 추가

--- app/frontend/playground/src/App.jsx
+++ app/frontend/playground/src/App.jsx
@@ -8,24 +8,24 @@ import GetTest from "./components/GetTest";
 
 const App = () => {
   return (
-    <div className="prose">
-      <h1>Authentication</h1>
+    <>
+      <div className="divider text-2xl font-bold">Authentication</div>
       <div className="flex p-4 gap-4">
         <Main />
         <SignUp />
         <Login />
       </div>
-      <h1>POST</h1>
+      <div className="divider text-2xl font-bold">POST</div>
       <div className="flex p-4 gap-4">
         <PostTest />
       </div>
-      <h1>GET</h1>
+      <div className="divider text-2xl font-bold">GET</div>
       <div className="flex p-4 gap-4">
         <GetDynamoTest />
         <GetS3Test />
         <GetTest />
       </div>
-    </div>
+    </>
   );
 };
 
