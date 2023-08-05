4ea9d0b3b6464445ed16e1a3f369543e070bea8c
조성원 <tjddnjsjo@kakao.com>
2023-08-05 11:21:42 +0900

    refactor: S3 API 수정

--- app/frontend/playground/src/apis/s3Api.js
+++ app/frontend/playground/src/apis/s3Api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-const ROUTE = "";
+const ROUTE = "image";
 
 const s3Instance = axios.create({
   baseURL:
-    "https://52ks59qzm5.execute-api.ap-northeast-2.amazonaws.com/default",
+    "https://52ks59qzm5.execute-api.ap-northeast-2.amazonaws.com/default", // TODO: Add your API Gateway URL
 });
 
 const postImage = async (fileName, formData) => {
-  return await s3Instance.post(`/image?filename=${fileName}`, formData, {
+  return await s3Instance.post(`${ROUTE}?filename=${fileName}`, formData, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
@@ -16,7 +16,7 @@ const postImage = async (fileName, formData) => {
 };
 
 const getImage = async (fileName) => {
-  return await s3Instance.get(`/image?filename=${fileName}`, {
+  return await s3Instance.get(`${ROUTE}?filename=${fileName}`, {
     headers: {
       "Content-Type": "application/json",
     },
