ff50c64ed4d86d77480a65297a3a07bc890f3db1
J2yun <alex68566856@gmail.com>
2023-08-07 21:56:11 +0900

    feat: Get image 추가

--- cdk/lib/stack/apigateway-stack.ts
+++ cdk/lib/stack/apigateway-stack.ts
@@ -80,7 +80,7 @@ export class PictatoApiGatewayStack extends cdk.Stack {
       },
     });
 
-    // POST {user-id}
+    // POST ./{user-id}
     const createRequest = userId.addMethod(
       "POST",
       new apigateway.LambdaIntegration(lambdaStack.lambdaCreatePostFunction, {
@@ -138,7 +138,7 @@ export class PictatoApiGatewayStack extends cdk.Stack {
       },
     });
 
-    // POST ./{user-id}/{index}
+    // DELETE ./{user-id}/{index}
     const deletePostRequest = index.addMethod(
       "DELETE",
       new apigateway.LambdaIntegration(lambdaStack.lambdaDeletePostFunction, {
@@ -234,6 +234,77 @@ export class PictatoApiGatewayStack extends cdk.Stack {
     );
 
     postImageRequest.addMethodResponse({
+      statusCode: "200",
+      responseParameters: {
+        "method.response.header.Access-Control-Allow-Origin": true,
+      },
+    });
+
+    // GET ./{user-id}/image
+    const getImageRequest = image.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(lambdaStack.lambdaGetImageFunction, {
+        proxy: false,
+        requestTemplates: {
+          "application/json": `
+          ##  See http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-mapping-template-reference.html
+          ##  This template will pass through all parameters including path, querystring, header, stage variables, and context through to the integration endpoint via the body/payload
+          #set($allParams = $input.params())
+          {
+          "body-json" : $input.json('$'),
+          "params" : {
+          #foreach($type in $allParams.keySet())
+              #set($params = $allParams.get($type))
+          "$type" : {
+              #foreach($paramName in $params.keySet())
+              "$paramName" : "$util.escapeJavaScript($params.get($paramName))"
+                  #if($foreach.hasNext),#end
+              #end
+          }
+              #if($foreach.hasNext),#end
+          #end
+          },
+          "stage-variables" : {
+          #foreach($key in $stageVariables.keySet())
+          "$key" : "$util.escapeJavaScript($stageVariables.get($key))"
+              #if($foreach.hasNext),#end
+          #end
+          },
+          "context" : {
+              "account-id" : "$context.identity.accountId",
+              "api-id" : "$context.apiId",
+              "api-key" : "$context.identity.apiKey",
+              "authorizer-principal-id" : "$context.authorizer.principalId",
+              "caller" : "$context.identity.caller",
+              "cognito-authentication-provider" : "$context.identity.cognitoAuthenticationProvider",
+              "cognito-authentication-type" : "$context.identity.cognitoAuthenticationType",
+              "cognito-identity-id" : "$context.identity.cognitoIdentityId",
+              "cognito-identity-pool-id" : "$context.identity.cognitoIdentityPoolId",
+              "http-method" : "$context.httpMethod",
+              "stage" : "$context.stage",
+              "source-ip" : "$context.identity.sourceIp",
+              "user" : "$context.identity.user",
+              "user-agent" : "$context.identity.userAgent",
+              "user-arn" : "$context.identity.userArn",
+              "request-id" : "$context.requestId",
+              "resource-id" : "$context.resourceId",
+              "resource-path" : "$context.resourcePath"
+              }
+          }
+          `,
+        },
+        integrationResponses: [
+          {
+            statusCode: "200",
+            responseParameters: {
+              "method.response.header.Access-Control-Allow-Origin": "'*'",
+            },
+          },
+        ],
+      })
+    );
+
+    getImageRequest.addMethodResponse({
       statusCode: "200",
       responseParameters: {
         "method.response.header.Access-Control-Allow-Origin": true,
