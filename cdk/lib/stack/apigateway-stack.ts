e2d0c66e6f73d681ec10f626a992589c0ebd35ab
J2yun <alex68566856@gmail.com>
2023-08-07 23:27:45 +0900

    refactor: Cognito 고려해서 dynamo쪽 mapping template 수정

--- cdk/lib/stack/apigateway-stack.ts
+++ cdk/lib/stack/apigateway-stack.ts
@@ -7,14 +7,9 @@ import { getAccountUniqueName } from "../config/accounts";
 import { SYSTEM_NAME } from "../config/commons";
 
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
-import {
-  ManagedPolicy,
-  Role,
-  ServicePrincipal,
-  CompositePrincipal,
-} from "aws-cdk-lib/aws-iam";
+
 import { PictatoLambdaStack } from "./lambda-stack";
-import { Service } from "aws-cdk-lib/aws-servicediscovery";
+import { requestTemplate } from "./mapping-template";
 
 export class PictatoApiGatewayStack extends cdk.Stack {
   constructor(
@@ -48,6 +43,7 @@ export class PictatoApiGatewayStack extends cdk.Stack {
       },
       binaryMediaTypes: ["multipart/form-data"],
     });
+
     const users = api.root.addResource("gallery");
     const userId = users.addResource("{user_id}");
     const index = userId.addResource("{index}");
@@ -58,9 +54,7 @@ export class PictatoApiGatewayStack extends cdk.Stack {
       new apigateway.LambdaIntegration(lambdaStack.lambdaReadPostFunction, {
         proxy: false,
         requestTemplates: {
-          "application/json": `{
-              "userId": "$input.params('user_id')"
-            }`,
+          "application/json": requestTemplate,
         },
         integrationResponses: [
           {
@@ -86,10 +80,7 @@ export class PictatoApiGatewayStack extends cdk.Stack {
       new apigateway.LambdaIntegration(lambdaStack.lambdaCreatePostFunction, {
         proxy: false,
         requestTemplates: {
-          "application/json": `{
-            "userId": "$input.params('user_id')",
-            "requestBody": $input.json("$")
-            }`,
+          "application/json": requestTemplate,
         },
         integrationResponses: [
           {
@@ -115,10 +106,7 @@ export class PictatoApiGatewayStack extends cdk.Stack {
       new apigateway.LambdaIntegration(lambdaStack.lambdaGetOneDataFunction, {
         proxy: false,
         requestTemplates: {
-          "application/json": `{
-              "userId": "$input.params('user_id')",
-              "index": "$input.params('index')"
-            }`,
+          "application/json": requestTemplate,
         },
         integrationResponses: [
           {
@@ -144,10 +132,7 @@ export class PictatoApiGatewayStack extends cdk.Stack {
       new apigateway.LambdaIntegration(lambdaStack.lambdaDeletePostFunction, {
         proxy: false,
         requestTemplates: {
-          "application/json": `{
-              "userId": "$input.params('user_id')",
-              "index": "$input.params('index')"
-            }`,
+          "application/json": requestTemplate,
         },
         integrationResponses: [
           {
@@ -175,52 +160,7 @@ export class PictatoApiGatewayStack extends cdk.Stack {
         contentHandling: apigateway.ContentHandling.CONVERT_TO_TEXT,
         proxy: false,
         requestTemplates: {
-          "multipart/form-data": `
-          ##  See http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-mapping-template-reference.html
-          ##  This template will pass through all parameters including path, querystring, header, stage variables, and context through to the integration endpoint via the body/payload
-          #set($allParams = $input.params())
-          {
-          "body-json" : $input.json('$'),
-          "params" : {
-          #foreach($type in $allParams.keySet())
-              #set($params = $allParams.get($type))
-          "$type" : {
-              #foreach($paramName in $params.keySet())
-              "$paramName" : "$util.escapeJavaScript($params.get($paramName))"
-                  #if($foreach.hasNext),#end
-              #end
-          }
-              #if($foreach.hasNext),#end
-          #end
-          },
-          "stage-variables" : {
-          #foreach($key in $stageVariables.keySet())
-          "$key" : "$util.escapeJavaScript($stageVariables.get($key))"
-              #if($foreach.hasNext),#end
-          #end
-          },
-          "context" : {
-              "account-id" : "$context.identity.accountId",
-              "api-id" : "$context.apiId",
-              "api-key" : "$context.identity.apiKey",
-              "authorizer-principal-id" : "$context.authorizer.principalId",
-              "caller" : "$context.identity.caller",
-              "cognito-authentication-provider" : "$context.identity.cognitoAuthenticationProvider",
-              "cognito-authentication-type" : "$context.identity.cognitoAuthenticationType",
-              "cognito-identity-id" : "$context.identity.cognitoIdentityId",
-              "cognito-identity-pool-id" : "$context.identity.cognitoIdentityPoolId",
-              "http-method" : "$context.httpMethod",
-              "stage" : "$context.stage",
-              "source-ip" : "$context.identity.sourceIp",
-              "user" : "$context.identity.user",
-              "user-agent" : "$context.identity.userAgent",
-              "user-arn" : "$context.identity.userArn",
-              "request-id" : "$context.requestId",
-              "resource-id" : "$context.resourceId",
-              "resource-path" : "$context.resourcePath"
-              }
-          }
-          `,
+          "multipart/form-data": requestTemplate,
         },
         integrationResponses: [
           {
@@ -246,52 +186,7 @@ export class PictatoApiGatewayStack extends cdk.Stack {
       new apigateway.LambdaIntegration(lambdaStack.lambdaGetImageFunction, {
         proxy: false,
         requestTemplates: {
-          "application/json": `
-          ##  See http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-mapping-template-reference.html
-          ##  This template will pass through all parameters including path, querystring, header, stage variables, and context through to the integration endpoint via the body/payload
-          #set($allParams = $input.params())
-          {
-          "body-json" : $input.json('$'),
-          "params" : {
-          #foreach($type in $allParams.keySet())
-              #set($params = $allParams.get($type))
-          "$type" : {
-              #foreach($paramName in $params.keySet())
-              "$paramName" : "$util.escapeJavaScript($params.get($paramName))"
-                  #if($foreach.hasNext),#end
-              #end
-          }
-              #if($foreach.hasNext),#end
-          #end
-          },
-          "stage-variables" : {
-          #foreach($key in $stageVariables.keySet())
-          "$key" : "$util.escapeJavaScript($stageVariables.get($key))"
-              #if($foreach.hasNext),#end
-          #end
-          },
-          "context" : {
-              "account-id" : "$context.identity.accountId",
-              "api-id" : "$context.apiId",
-              "api-key" : "$context.identity.apiKey",
-              "authorizer-principal-id" : "$context.authorizer.principalId",
-              "caller" : "$context.identity.caller",
-              "cognito-authentication-provider" : "$context.identity.cognitoAuthenticationProvider",
-              "cognito-authentication-type" : "$context.identity.cognitoAuthenticationType",
-              "cognito-identity-id" : "$context.identity.cognitoIdentityId",
-              "cognito-identity-pool-id" : "$context.identity.cognitoIdentityPoolId",
-              "http-method" : "$context.httpMethod",
-              "stage" : "$context.stage",
-              "source-ip" : "$context.identity.sourceIp",
-              "user" : "$context.identity.user",
-              "user-agent" : "$context.identity.userAgent",
-              "user-arn" : "$context.identity.userArn",
-              "request-id" : "$context.requestId",
-              "resource-id" : "$context.resourceId",
-              "resource-path" : "$context.resourcePath"
-              }
-          }
-          `,
+          "application/json": requestTemplate,
         },
         integrationResponses: [
           {
