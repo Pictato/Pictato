3eb49bbfa463fa1021ae124838341dd47fbd270d
J2yun <alex68566856@gmail.com>
2023-08-10 02:37:47 +0900

    feat: lambda함수 내부 user 인증 로직 추가

--- app/backend/dynamodb/create-post/create-post.py
+++ app/backend/dynamodb/create-post/create-post.py
@@ -10,31 +10,42 @@ def lambda_handler(event, context):
 
     dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table(target_table)
+    cognito = boto3.client("cognito-idp")
 
     try:
+        access_token = event["params"]["header"]["access-token"]
+        cognito_user = cognito.get_user(AccessToken=access_token)
+        coginto_user_id = cognito_user["Username"]
+
         index = str(int(time.time()))
         date = str(datetime.fromtimestamp(int(time.time())))
-        userID = event["params"]["path"]["user_id"]
+        user_id = event["params"]["path"]["user_id"]
 
         fileName = event["body-json"]["fileName"]
         memo = event["body-json"]["memo"]
     except:
         return {
             "statusCode": 500,
-            "body": json.dumps("Data loading error!"),
+            "body": json.dumps("Data loading or get_user error!"),
         }
 
-    try:
-        table.put_item(
-            Item={
-                "user-id": userID,
-                "index": index,
-                "file-name": fileName,
-                "memo": memo,
-                "date": date,
-            }
-        )
-    except:
-        return {"statusCode": 500, "body": json.dumps("put item error!!")}
+    if coginto_user_id == user_id:
+        try:
+            table.put_item(
+                Item={
+                    "user-id": user_id,
+                    "index": index,
+                    "file-name": fileName,
+                    "memo": memo,
+                    "date": date,
+                }
+            )
+        except:
+            return {"statusCode": 500, "body": json.dumps("put item error!!")}
 
-    return {"statusCode": 200, "body": json.dumps("Add in DB successful!!")}
+        return {"statusCode": 200, "body": json.dumps("Add in DB successful!!")}
+    else:
+        return {
+            "statusCode": 500,
+            "body": json.dumps("You tried to upload to another User!!"),
+        }
