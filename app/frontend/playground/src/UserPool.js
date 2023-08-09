c4d992e8d8bb896b6a7d3632788b29e7bb8c3de5
J2yun <alex68566856@gmail.com>
2023-08-10 03:03:27 +0900

    refactor: Cognito UserPool 변경

--- app/frontend/playground/src/UserPool.js
+++ app/frontend/playground/src/UserPool.js
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 
 const poolData = {
-  UserPoolId: "ap-northeast-2_Ovo61V5vt", // Your user pool id here
-  ClientId: "24vrvmerh3itpvt4tnl05tpltu", // Your client id here
+  UserPoolId: "ap-northeast-2_Ji4iOBdqz", // Your user pool id here
+  ClientId: "1hdhfrc1tuca16q5kip5l3vame", // Your client id here
 };
 
 export default new CognitoUserPool(poolData);
