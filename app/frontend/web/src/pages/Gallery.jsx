969d6b4d05cb67364b905215cd98265c30379551
조성원 <tjddnjsjo@kakao.com>
2023-08-15 00:36:13 +0900

    feat: Navbar 추가

--- app/frontend/web/src/pages/Gallery.jsx
+++ app/frontend/web/src/pages/Gallery.jsx
@@ -1,21 +1,36 @@
-import { useEffect, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import AccountContext from "../contexts/account-context";
 import { dynamoApi } from "../apis/dynamoApi";
+import Navbar from "../components/ui/Navbar";
 
 const Gallery = () => {
-  const { username } = useParams();
+  const { space } = useParams();
+
+  const { getSession, setUsername } = useContext(AccountContext);
   const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getSession().then((session) => {
+      setUsername(session.idToken.payload["cognito:username"]);
+    });
+  }, []);
 
   useEffect(() => {
     handleGetRequest();
   }, []);
 
   const handleGetRequest = async () => {
-    const res = await dynamoApi.readAllPost(username);
+    const res = await dynamoApi.readAllPost(space);
     setData(res.data.body);
   };
 
-  return console.log(data);
+  return (
+    <div className="p-4">
+      <Navbar space={space} />
+      {console.log(data)}
+    </div>
+  );
 };
 
 export default Gallery;
