8186d145cfc2d5bbc0398ff5000e73541a454345
조성원 <tjddnjsjo@kakao.com>
2023-08-15 04:49:19 +0900

    feat: 폴라로이드 사진 추가

--- app/frontend/web/src/pages/Gallery.jsx
+++ app/frontend/web/src/pages/Gallery.jsx
@@ -2,25 +2,43 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { galleryApi } from "../apis/galleryApi";
 import Navbar from "../components/ui/Navbar";
+import "../styles/kccchassam.css";
 
 const Gallery = () => {
   const { space } = useParams();
 
-  const [data, setData] = useState([]);
+  const [gallery, setGallery] = useState([]);
 
   useEffect(() => {
-    handleGetRequest();
+    const fetchGallery = async () => {
+      const res = await galleryApi.getAllPosts(space);
+      setGallery(res.data.body);
+    };
+
+    fetchGallery();
   }, []);
 
-  const handleGetRequest = async () => {
-    const res = await galleryApi.getAllPosts(space);
-    setData(res.data.body);
-  };
-
   return (
-    <div className="p-4">
+    <div className="flex flex-col gap-4 p-4">
       <Navbar space={space} />
-      {console.log(data)}
+      <div className="flex">
+        {gallery.map((piece) => (
+          <div
+            className="card card-compact rounded-none bg-white shadow-xl"
+            key={piece["file-name"]}
+          >
+            <figure className="px-4 pt-4">
+              <img
+                src={`https://team2-icn-pictato-bucket.s3.ap-northeast-2.amazonaws.com/${space}/${piece["file-name"]}`}
+                alt={piece["file-name"]}
+              />
+            </figure>
+            <div className="card-body mt-6 items-end kcc-chassam">
+              <p className="text-xl">{piece.memo}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
