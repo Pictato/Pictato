24431cb71b33a7d80af6a68ce12dc09fdf6d5b94
조성원 <tjddnjsjo@kakao.com>
2023-08-16 15:16:30 +0900

    feat: Masonry 적용

--- app/frontend/web/src/pages/Gallery.jsx
+++ app/frontend/web/src/pages/Gallery.jsx
@@ -2,6 +2,7 @@ import { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import AccountContext from "../contexts/account-context";
 import { galleryApi } from "../apis/galleryApi";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import Navbar from "../components/ui/Navbar";
 import { AiOutlineDelete } from "react-icons/ai";
 import "../styles/kccchassam.css";
@@ -35,36 +36,47 @@ const Gallery = () => {
   return (
     <div className="flex flex-col gap-4 p-4 preview">
       <Navbar space={space} />
-      <div className="flex flex-wrap items-start gap-4">
-        {gallery.map((piece) => (
-          <div
-            className="card card-compact rounded-none bg-white shadow-xl"
-            key={piece["file-name"]}
-          >
-            <figure className="px-4 pt-4">
-              <img
-                src={`https://team2-icn-pictato-bucket.s3.ap-northeast-2.amazonaws.com/${space}/${piece["file-name"]}`}
-                alt={piece["file-name"]}
-              />
-            </figure>
-            <div className="card-body mt-2 items-end kcc-chassam">
-              <div className="flex items-center gap-4">
-                {username === space && (
-                  <button
-                    className="btn btn-error btn-square"
-                    onClick={() => {
-                      handleDeleteRequest(piece.index);
-                    }}
-                  >
-                    <AiOutlineDelete size="24" />
-                  </button>
-                )}
-                <p className="text-xl">{piece.memo}</p>
+      <ResponsiveMasonry
+        columnsCountBreakPoints={{
+          0: 1,
+          350: 2,
+          700: 3,
+          1050: 4,
+          1400: 5,
+          1750: 6,
+        }}
+      >
+        <Masonry gutter="1rem">
+          {gallery.map((piece) => (
+            <div
+              className="card card-compact rounded-none bg-white shadow-xl"
+              key={piece["file-name"]}
+            >
+              <figure className="px-4 pt-4">
+                <img
+                  src={`https://team2-icn-pictato-bucket.s3.ap-northeast-2.amazonaws.com/${space}/${piece["file-name"]}`}
+                  alt={piece["file-name"]}
+                />
+              </figure>
+              <div className="card-body mt-2 items-end kcc-chassam">
+                <div className="flex items-center gap-4">
+                  {username === space && (
+                    <button
+                      className="btn btn-error btn-square"
+                      onClick={() => {
+                        handleDeleteRequest(piece.index);
+                      }}
+                    >
+                      <AiOutlineDelete size="24" />
+                    </button>
+                  )}
+                  <p className="text-xl">{piece.memo}</p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </Masonry>
+      </ResponsiveMasonry>
     </div>
   );
 };
