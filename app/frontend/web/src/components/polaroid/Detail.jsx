a82d7d54a8afbd67ee05b887749fe949b3fe8c35
조성원 <tjddnjsjo@kakao.com>
2023-08-19 21:24:41 +0900

    feat: 삭제 진행 표시

--- app/frontend/web/src/components/polaroid/Detail.jsx
+++ app/frontend/web/src/components/polaroid/Detail.jsx
@@ -15,6 +15,7 @@ const trans = (x, y, s) =>
 const Detail = ({ space, piece }) => {
   const { username, getIdToken, getAccessToken } = useContext(AccountContext);
   const [isFlipped, setIsFlipped] = useState(false);
+  const [isProcessing, setIsProcessing] = useState(false);
 
   const [props, set] = useSpring(() => ({
     xys: [0, 0, 1],
@@ -35,63 +36,76 @@ const Detail = ({ space, piece }) => {
 
   return (
     <dialog id={piece["file-name"]} className="modal">
-      <form method="dialog" className="flex flex-col items-center">
-        <animated.div
-          className="[perspective:1000px]"
-          onMouseMove={({ clientX: x, clientY: y }) =>
-            set.start({ xys: calc(x, y) })
-          }
-          onMouseLeave={() => set({ xys: [0, 0, 1] })}
-          style={{
-            transform: props.xys.to(trans),
-          }}
-        >
-          <div
-            className={`cursor-pointer transition-all duration-500 [transform-style:preserve-3d] card card-compact rounded-none bg-white shadow-xl ${
-              isFlipped && "[transform:rotateY(180deg)]"
-            }`}
-            onClick={() => setIsFlipped((cur) => !cur)}
+      <div className="flex flex-col items-center">
+        <form method="dialog">
+          <animated.div
+            className="[perspective:1000px]"
+            onMouseMove={({ clientX: x, clientY: y }) =>
+              set.start({ xys: calc(x, y) })
+            }
+            onMouseLeave={() => set({ xys: [0, 0, 1] })}
+            style={{
+              transform: props.xys.to(trans),
+            }}
           >
-            <div className="[backface-visibility:hidden]">
-              <figure className="px-4 pt-4">
-                <img
-                  src={`https://team2-icn-pictato-bucket.s3.ap-northeast-2.amazonaws.com/before-resize/${space}/${piece["file-name"]}`}
-                  className="max-h-[calc(100vh-400px)]"
-                  alt={piece["file-name"]}
-                />
-              </figure>
-              <div className="card-body mt-2 items-end kcc-chassam">
-                <p className="text-xl">{piece.memo}</p>
+            <div
+              className={`cursor-pointer transition-all duration-500 [transform-style:preserve-3d] card card-compact rounded-none bg-white shadow-xl ${
+                isFlipped && "[transform:rotateY(180deg)]"
+              }`}
+              onClick={() => setIsFlipped((cur) => !cur)}
+            >
+              <div className="[backface-visibility:hidden]">
+                <figure className="px-4 pt-4">
+                  <img
+                    src={`https://team2-icn-pictato-bucket.s3.ap-northeast-2.amazonaws.com/before-resize/${space}/${piece["file-name"]}`}
+                    className="max-h-[calc(100vh-400px)]"
+                    alt={piece["file-name"]}
+                  />
+                </figure>
+                <div className="card-body mt-2 items-end kcc-chassam">
+                  <p className="text-xl">{piece.memo}</p>
+                </div>
+              </div>
+              <div className="absolute inset-0 [backface-visibility:hidden] [transform:rotateY(180deg)]">
+                <div className="card-body kcc-chassam">
+                  <p className="truncate">date: {piece.date}</p>
+                  <p className="truncate">file-name: {piece["file-name"]}</p>
+                  <p className="truncate">index: {piece.index}</p>
+                  <p className="truncate">memo: {piece.memo}</p>
+                  <p className="truncate">owner: {piece["user-id"]}</p>
+                </div>
               </div>
             </div>
-            <div className="absolute inset-0 [backface-visibility:hidden] [transform:rotateY(180deg)]">
-              <div className="card-body kcc-chassam">
-                <p className="truncate">date: {piece.date}</p>
-                <p className="truncate">file-name: {piece["file-name"]}</p>
-                <p className="truncate">index: {piece.index}</p>
-                <p className="truncate">memo: {piece.memo}</p>
-                <p className="truncate">owner: {piece["user-id"]}</p>
-              </div>
-            </div>
-          </div>
-        </animated.div>
+          </animated.div>
+        </form>
         <div className="modal-action">
           {username === space && (
             <button
               className="btn btn-error"
               onClick={() => {
+                setIsProcessing(true);
                 handleDeleteRequest(piece.index);
               }}
+              disabled={isProcessing}
             >
-              <AiOutlineDelete size="16" /> 삭제
+              {isProcessing ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                <>
+                  <AiOutlineDelete size="16" /> 삭제
+                </>
+              )}
             </button>
           )}
-          <button className="btn">
+          <button
+            className="btn"
+            onClick={() => window[piece["file-name"]].close()}
+          >
             <AiOutlineClose size="16" />
             닫기
           </button>
         </div>
-      </form>
+      </div>
     </dialog>
   );
 };
