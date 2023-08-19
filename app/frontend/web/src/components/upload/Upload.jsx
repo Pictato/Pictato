f13820b2bb453efc3c43c01e8eb762a29c2c3224
조성원 <tjddnjsjo@kakao.com>
2023-08-19 21:30:33 +0900

    feat: 업로드 진행 표시

--- app/frontend/web/src/components/upload/Upload.jsx
+++ app/frontend/web/src/components/upload/Upload.jsx
@@ -1,4 +1,4 @@
-import { useRef, useContext } from "react";
+import { useContext, useRef, useState } from "react";
 import AccountContext from "../../contexts/account-context";
 import { galleryApi } from "../../apis/galleryApi";
 
@@ -6,6 +6,7 @@ const Upload = () => {
   const { username, getIdToken, getAccessToken } = useContext(AccountContext);
   const fileRef = useRef();
   const memoRef = useRef();
+  const [isProcessing, setIsProcessing] = useState(false);
 
   const handlePostRequest = async () => {
     try {
@@ -26,27 +27,40 @@ const Upload = () => {
 
   return (
     <dialog id="upload_modal" className="modal">
-      <form method="dialog" className="modal-box">
-        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-          ✕
-        </button>
-        <h3 className="font-bold text-lg">업로드</h3>
-        <input
-          type="file"
-          className="file-input file-input-bordered w-full mt-2"
-          ref={fileRef}
-        />
-        <textarea
-          className="textarea textarea-lg textarea-bordered w-full mt-2"
-          placeholder="메모"
-          ref={memoRef}
-        />
+      <div className="modal-box">
+        <form method="dialog">
+          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            ✕
+          </button>
+          <h3 className="font-bold text-lg">업로드</h3>
+          <input
+            type="file"
+            className="file-input file-input-bordered w-full mt-2"
+            ref={fileRef}
+          />
+          <textarea
+            className="textarea textarea-lg textarea-bordered w-full mt-2"
+            placeholder="메모"
+            ref={memoRef}
+          />
+        </form>
         <div className="modal-action">
-          <button className="btn btn-primary" onClick={handlePostRequest}>
-            게시
+          <button
+            className="btn btn-primary"
+            onClick={() => {
+              setIsProcessing(true);
+              handlePostRequest();
+            }}
+            disabled={isProcessing}
+          >
+            {isProcessing ? (
+              <span className="loading loading-spinner"></span>
+            ) : (
+              "게시"
+            )}
           </button>
         </div>
-      </form>
+      </div>
     </dialog>
   );
 };
